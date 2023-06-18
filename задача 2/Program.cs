e899ac4a0d87d2ae770e2be9fe27a4536452d873
unknown <lidianegoda5@gmail.com>
2023-06-18 22:52:18 +0300

    Решение задачи 2

---
+++ задача 2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.Clear();
+Console.Write("a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int b = int.Parse(Console.ReadLine());
+if (a>b) Console.WriteLine("a - max");
+else if (a<b) Console.WriteLine("b - max");
---
+++ задача 4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.Clear();
+Console.Write("a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b:");
+int b = int.Parse(Console.ReadLine());
+Console.Write("c:");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Max(a, Math.Max(b, c)));
